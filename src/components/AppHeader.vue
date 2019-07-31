c9a71ba0106ee84ca9bc74914196523bbb323227
Ann Kiriakidi <anuitka1999@gmail.com>
2019-07-31 13:43:14 +0300

    vuetify

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+	<v-app>
+		<v-toolbar app class="primary">
+		</v-toolbar>
+	</v-app>
+</template>
+
+<script>
+export default {
+
+  name: 'AppHeader',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
