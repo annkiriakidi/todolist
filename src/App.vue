0d563c3d8fdd65e8f14ac8b4c382b57f3138ecca
Ann Kiriakidi <anuitka1999@gmail.com>
2019-08-04 15:26:35 +0300

    underloaded files

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,99 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <AppHeader/>
+      <AppForm :todo="todo"  @addTodo="addTodo"/>
+      <v-conteiner grid-list-md fluid>
+        <v-layout wrap align-content-center align-center justify-start>
+        <v-flex xs2 v-for="todo in todos" :key="todo.id">
+      <OneItem 
+        :todo="todo"
+        :complited="todo.complited"
+        @removeTodo="removeTodo"/>
+      </v-flex>
+
+        </v-layout>
+      </v-conteiner>
+    <AppFooter/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import AppHeader from './components/AppHeader';
+import AppForm from './components/AppForm';
+import OneItem from './components/OneItem';
+import AppFooter from './components/AppFooter';
+
+
+const presetValues = [{
+  "title":"Task 1",
+  "completed":true
+}, {
+  "title":"Task 2",
+  "completed":false
+}, {
+  "title":"Task 3",
+  "completed":false
+}]
+
+const STORAGE_KEY = "todo-app";
+const todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY)) || presetValues;
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    AppHeader,
+    OneItem,
+    AppFooter,
+    AppForm,
   },
-  data: () => ({
-    //
-  }),
+  
+  data() {
+    return{
+        todos: todoStorage.fetch(),
+        todo: "",
+    }
+  },
+
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos);
+      },
+      deep: true
+    }
+  },
+
+  methods: {
+    addTodo (todo) {
+      var value = todo && todo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        title: value,
+        completed: false,
+      });
+      todo = " ";
+    },
+
+    removeTodo: function(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import vuetify from './plugins/vuetify';
@@ -12,8 +11,10 @@ Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  
 }).$mount('#app')
